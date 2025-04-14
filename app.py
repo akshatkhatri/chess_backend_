164a09e6dfb3a856b328ce4f65965390d346d20e
Akshat Khatri <jitikhatri@gmail.com>
2025-04-14 17:08:52 +0530

    Virtual Env Changes

--- app.py
+++ app.py
@@ -95,5 +95,7 @@ def uci_command():
 #     port = int(os.environ.get("PORT", 5000))
 #     app.run(debug=False, host='0.0.0.0', port=port, threaded=True)
 
-# if __name__ == "__main__":
-#     app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    # Use the port from the environment variable, default to 5000 if not set
+    port = int(os.getenv("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
